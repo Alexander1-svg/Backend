438c486cf6af21300b10615c6e9afa0bba66ce82
Alexander1-svg <al.bellod@duocuc.cl>
2025-11-13 03:37:26 -0300

    Fix: Preparando conexión con la base de datos

---
+++ src/main/java/com/backendLevelup/Backend/init/DataInitializer.java
@@ -0,0 +1,14 @@
+package com.backendLevelup.Backend.init;
+
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.logging.Logger;
+
+public class DataInitializer implements CommandLineRunner {
+
+    private static final Logger logger = Logger.getLogger(String.valueOf(DataInitializer.class));
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
