5c6615fac232a84875dd9be0bd8f0d3653172247
Abraham2143 <ab.neira@duocuc.cl>
2025-11-21 21:03:32 -0300

    Implementacion de optional

--- src/main/java/com/backendLevelup/Backend/repository/ProductoRepository.java
+++ src/main/java/com/backendLevelup/Backend/repository/ProductoRepository.java
@@ -4,7 +4,10 @@ import com.backendLevelup.Backend.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoRepository extends JpaRepository<Producto,Long> {
     List<Producto> findByCategoriaNombre(String nombreCategoria);
+
+    Optional<Producto> findByNombre(String nombre);
 }
--- src/main/java/com/backendLevelup/Backend/repository/UsuarioRepository.java
+++ src/main/java/com/backendLevelup/Backend/repository/UsuarioRepository.java
@@ -1,13 +1,15 @@
 package com.backendLevelup.Backend.repository;
 
 import com.backendLevelup.Backend.model.Usuario;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     // Busca un usuario al hacer login
     Optional<Usuario> findByEmail(String email);
+    // Busca un usuario por el name
+    Optional<Usuario> findByUsername(String username);
     // Para verificar si un email ya esta registrado
     boolean existsByEmail(String email);
 }
